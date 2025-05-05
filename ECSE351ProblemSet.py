905a142ffc32d7fefc23a6b776cf8da8babdf795
pieterverbeek <pieterverbeek@Pieters-Mac-2.local>
2025-05-05 10:27:06 -0400

    Update before changing strategies

--- ECSE351ProblemSet.py
+++ ECSE351ProblemSet.py
@@ -63,7 +63,8 @@ class ECSE351ProblemSet(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 500000
+        self.samp_rate = samp_rate = 200000
+        self.relative_scale_dB = relative_scale_dB = 0
         self.mod_index = mod_index = .5
         self.f_mod = f_mod = 1000
         self.f_carrier = f_carrier = 10000
@@ -82,45 +83,73 @@ class ECSE351ProblemSet(gr.top_block, Qt.QWidget):
         self._f_carrier_range = qtgui.Range(5000, 15000, .1, 10000, 200)
         self._f_carrier_win = qtgui.RangeWidget(self._f_carrier_range, self.set_f_carrier, "'f_carrier'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._f_carrier_win)
+        self._relative_scale_dB_range = qtgui.Range(0, 60, 5, 0, 200)
+        self._relative_scale_dB_win = qtgui.RangeWidget(self._relative_scale_dB_range, self.set_relative_scale_dB, "'relative_scale_dB'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._relative_scale_dB_win)
         self.qtgui_sink_x_0 = qtgui.sink_f(
             1024, #fftsize
-            window.WIN_KAISER, #wintype
+            window.WIN_BLACKMAN_hARRIS, #wintype
             0, #fc
             samp_rate, #bw
             "", #name
             True, #plotfreq
             True, #plotwaterfall
             True, #plottime
-            False, #plotconst
+            True, #plotconst
             None # parent
         )
         self.qtgui_sink_x_0.set_update_time(1.0/10)
         self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.qwidget(), Qt.QWidget)
 
-        self.qtgui_sink_x_0.enable_rf_freq(True)
+        self.qtgui_sink_x_0.enable_rf_freq(False)
 
         self.top_layout.addWidget(self._qtgui_sink_x_0_win)
-        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
-        self.blocks_multiply_xx_1 = blocks.multiply_vff(1)
-        self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
-        self.blocks_add_xx_0 = blocks.add_vff(1)
-        self.analog_sig_source_x_1 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, f_carrier, 1, 0, 0)
-        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, f_mod, 1, 0, 0)
-        self.analog_const_source_x_1 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 1.0)
-        self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, mod_index)
+        self.blocks_throttle2_0_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_multiply_xx_1_0 = blocks.multiply_vcc(1)
+        self.blocks_multiply_xx_1 = blocks.multiply_vcc(1)
+        self.blocks_multiply_xx_0_0 = blocks.multiply_vcc(1)
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.blocks_add_xx_1 = blocks.add_vcc(1)
+        self.blocks_add_xx_0_0 = blocks.add_vcc(1)
+        self.blocks_add_xx_0 = blocks.add_vcc(1)
+        self.analog_sig_source_x_1_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, f_carrier, 1, 0, 0)
+        self.analog_sig_source_x_1 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, f_carrier, 1, 0, 0)
+        self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 200, 1, 0, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, f_mod, 1, 0, 0)
+        self.analog_const_source_x_1_0 = analog.sig_source_c(0, analog.GR_CONST_WAVE, 0, 0, 1.0)
+        self.analog_const_source_x_1 = analog.sig_source_c(0, analog.GR_CONST_WAVE, 0, 0, 1.0)
+        self.analog_const_source_x_0_0 = analog.sig_source_c(0, analog.GR_CONST_WAVE, 0, 0, mod_index)
+        self.analog_const_source_x_0 = analog.sig_source_c(0, analog.GR_CONST_WAVE, 0, 0, mod_index)
+        self.analog_am_demod_cf_0 = analog.am_demod_cf(
+        	channel_rate=samp_rate,
+        	audio_decim=1,
+        	audio_pass=5000,
+        	audio_stop=5500,
+        )
 
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.analog_am_demod_cf_0, 0), (self.qtgui_sink_x_0, 0))
         self.connect((self.analog_const_source_x_0, 0), (self.blocks_multiply_xx_1, 1))
+        self.connect((self.analog_const_source_x_0_0, 0), (self.blocks_multiply_xx_1_0, 1))
         self.connect((self.analog_const_source_x_1, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.analog_const_source_x_1_0, 0), (self.blocks_add_xx_0_0, 1))
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_1, 0))
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.blocks_multiply_xx_1_0, 0))
         self.connect((self.analog_sig_source_x_1, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.analog_sig_source_x_1_0, 0), (self.blocks_multiply_xx_0_0, 1))
         self.connect((self.blocks_add_xx_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.blocks_add_xx_0_0, 0), (self.blocks_multiply_xx_0_0, 0))
+        self.connect((self.blocks_add_xx_1, 0), (self.analog_am_demod_cf_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.blocks_multiply_xx_0_0, 0), (self.blocks_throttle2_0_0, 0))
         self.connect((self.blocks_multiply_xx_1, 0), (self.blocks_add_xx_0, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.blocks_multiply_xx_1_0, 0), (self.blocks_add_xx_0_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.blocks_add_xx_1, 0))
+        self.connect((self.blocks_throttle2_0_0, 0), (self.blocks_add_xx_1, 1))
 
 
     def closeEvent(self, event):
@@ -137,9 +166,18 @@ class ECSE351ProblemSet(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
         self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_1_0.set_sampling_freq(self.samp_rate)
         self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+        self.blocks_throttle2_0_0.set_sample_rate(self.samp_rate)
         self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
+
+    def get_relative_scale_dB(self):
+        return self.relative_scale_dB
+
+    def set_relative_scale_dB(self, relative_scale_dB):
+        self.relative_scale_dB = relative_scale_dB
 
     def get_mod_index(self):
         return self.mod_index
@@ -147,6 +185,7 @@ class ECSE351ProblemSet(gr.top_block, Qt.QWidget):
     def set_mod_index(self, mod_index):
         self.mod_index = mod_index
         self.analog_const_source_x_0.set_offset(self.mod_index)
+        self.analog_const_source_x_0_0.set_offset(self.mod_index)
 
     def get_f_mod(self):
         return self.f_mod
@@ -161,6 +200,7 @@ class ECSE351ProblemSet(gr.top_block, Qt.QWidget):
     def set_f_carrier(self, f_carrier):
         self.f_carrier = f_carrier
         self.analog_sig_source_x_1.set_frequency(self.f_carrier)
+        self.analog_sig_source_x_1_0.set_frequency(self.f_carrier)
 
     def get_AM_amp(self):
         return self.AM_amp
